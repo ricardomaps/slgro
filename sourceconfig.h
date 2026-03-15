e88bc966426958e4b242b549ae5ef30242dc9d18
0uppy <0uppy@noreply.localhost>
2026-03-15 14:57:33 +0000

    slgro 1.1 - the cleanup update
    Removed unused code that wasn't used for slgro

--- sourceconfig.h
+++ sourceconfig.h
@@ -9,27 +9,38 @@
 
 static const struct config cfg = {
 	.motion_throttle_hz = 85,
-	.border_col_active = 0xffed953e,
-	.border_col_normal = 0xff444444,
-	.border_width = 1,
-	.gaps = 0,
+	.border_col_active = 0xe8e0d8ff,
+	.border_col_normal = 0x00000000,
+	.border_width = 2,
 };
 
-static const char* termcmd[] = { "st-wl", NULL };
+static const char* termcmd[]       = { "havoc", NULL };
+static const char* menucmd[]       = { "/home/dani/Scripts/launcher.sh", NULL };
+static const char* browsercmd[]    = { "librewolf-bin", NULL };
+static const char* filescmd[]      = { "pcmanfm", NULL };
+static const char* btcmd[]         = { "blueman-manager", NULL };
+static const char* volcmd[]        = { "pavucontrol", NULL };
+static const char* screenshotcmd[] = { "/home/dani/Scripts/screenshot.sh", NULL };
+
 static struct bind binds[] = {
-	{ SWC_BINDING_KEY,    MOD4,        XKB_KEY_Return, { .v = termcmd }, spawn },
-	{ SWC_BINDING_KEY,    MOD4|SHFT,   XKB_KEY_q,      { .v = NULL },    quit },
+	{ SWC_BINDING_KEY,    MOD4,        XKB_KEY_q,      { .v = termcmd },       spawn },
+	{ SWC_BINDING_KEY,    MOD4,        XKB_KEY_d,      { .v = menucmd },       spawn },
+	{ SWC_BINDING_KEY,    MOD4,        XKB_KEY_w,      { .v = browsercmd },    spawn },
+	{ SWC_BINDING_KEY,    MOD4,        XKB_KEY_e,      { .v = filescmd },      spawn },
+	{ SWC_BINDING_KEY,    MOD4,        XKB_KEY_b,      { .v = btcmd },         spawn },
+	{ SWC_BINDING_KEY,    MOD4,        XKB_KEY_v,      { .v = volcmd },        spawn },
+	{ SWC_BINDING_KEY,    MOD4|SHFT,   XKB_KEY_s,      { .v = screenshotcmd }, spawn },
 	{ SWC_BINDING_KEY,    MOD4,        XKB_KEY_Tab,    { .v = NULL },    focus_next },
 	{ SWC_BINDING_KEY,    MOD4,        XKB_KEY_f,      { .v = NULL },    fullscreen },
-	{ SWC_BINDING_KEY,    MOD4,        XKB_KEY_q,      { .v = NULL },    kill_sel },
-	{ SWC_BINDING_KEY,    MOD4,        XKB_KEY_Right,  { .i =  100 },     kb_move_x },
-	{ SWC_BINDING_KEY,    MOD4,        XKB_KEY_Left,   { .i = -100 },     kb_move_x },
-	{ SWC_BINDING_KEY,    MOD4,        XKB_KEY_Down,   { .i =  100 },     kb_move_y },
-	{ SWC_BINDING_KEY,    MOD4,        XKB_KEY_Up,     { .i = -100 },     kb_move_y },
+	{ SWC_BINDING_KEY,    MOD4,        XKB_KEY_c,      { .v = NULL },    kill_sel },
+	{ SWC_BINDING_KEY,    MOD4,        XKB_KEY_Right,  { .i = 75 },     kb_move_x },
+	{ SWC_BINDING_KEY,    MOD4,        XKB_KEY_Left,   { .i = -75 },     kb_move_x },
+	{ SWC_BINDING_KEY,    MOD4,        XKB_KEY_Down,   { .i = 75 },     kb_move_y },
+	{ SWC_BINDING_KEY,    MOD4,        XKB_KEY_Up,     { .i = -75 },     kb_move_y },
 	{ SWC_BINDING_KEY,    MOD4,        XKB_KEY_space,  { .v = NULL },    center_window },
-	{ SWC_BINDING_KEY,    MOD4|SHFT,   XKB_KEY_Right,  { .i =  25 },     kb_resize_width },
+	{ SWC_BINDING_KEY,    MOD4|SHFT,   XKB_KEY_Right,  { .i = 25 },     kb_resize_width },
 	{ SWC_BINDING_KEY,    MOD4|SHFT,   XKB_KEY_Left,   { .i = -25 },     kb_resize_width },
-	{ SWC_BINDING_KEY,    MOD4|SHFT,   XKB_KEY_Down,   { .i =  25 },     kb_resize_height },
+	{ SWC_BINDING_KEY,    MOD4|SHFT,   XKB_KEY_Down,   { .i = 25 },     kb_resize_height },
 	{ SWC_BINDING_KEY,    MOD4|SHFT,   XKB_KEY_Up,     { .i = -25 },     kb_resize_height },
 	{ SWC_BINDING_KEY,    MOD4,        XKB_KEY_h,      { .v = NULL },    snap_left_half },
 	{ SWC_BINDING_KEY,    MOD4,        XKB_KEY_l,      { .v = NULL },    snap_right_half },
